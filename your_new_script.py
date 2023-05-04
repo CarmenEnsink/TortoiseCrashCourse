3ad874eb05dc17dc78c1f437c3b87db75972048b
SMKCarmen <61141983+SMKCarmen@users.noreply.github.com>
2023-05-04 17:03:06 +0200

    Added a function to "your_new_script.py"

---
+++ your_new_script.py
@@ -0,0 +1,8 @@
+def yourfunction(input):
+	# Comment on what kind of data your input should to be: input should be str datatype
+	# Comment on what kind of data your output is going to be: output is str datatype
+	# Comment on what you do within this function: add two strings together
+
+	output = "do something with " + input
+
+	return output
