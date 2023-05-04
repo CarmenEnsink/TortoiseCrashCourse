4f7553c31c5bd138f9e45908c515b149fc4c50d9
CarmenEnsink <61141983+CarmenEnsink@users.noreply.github.com>
2023-05-04 17:07:15 +0200

    Update your_new_script.py

--- your_new_script.py
+++ your_new_script.py
@@ -1,8 +1,8 @@
 def yourfunction(input_1, input_2):
 	# Comment on what kind of data your input should to be: input should be of float datatype
 	# Comment on what kind of data your output is going to be: output is float datatype
-	# Comment on what you do within this function: add two numbers
+	# Comment on what you do within this function: multiply two numbers
 
-	output = input_1 + input_2 
+	output = input_1 * input_2 
 
 	return output
