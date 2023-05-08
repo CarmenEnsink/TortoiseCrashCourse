53e989ffdf86c12ea5b0099dc9c6180d63074a41
CarmenEnsink <61141983+CarmenEnsink@users.noreply.github.com>
2023-05-08 09:26:34 +0200

    Always provide a comment with your modifications.

--- your_new_script.py
+++ your_new_script.py
@@ -3,6 +3,4 @@ def yourfunction(input_1, input_2):
 	# Comment on what kind of data your output is going to be: output is float datatype
 	# Comment on what you do within this function: multiply two numbers
 
-	output = input_1 * input_2 
-
-	return output
+	return input_1 * input_2
